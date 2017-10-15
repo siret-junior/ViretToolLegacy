45a32989e8fd7941dbe61f575be0d0b923a5bf17
soCzech <soucek.gns@gmail.com>
2017-10-15 21:03:08 +0200

    Generic IComparable.

--- ViretTool/SimilarityModels/RankedResult.cs
+++ ViretTool/SimilarityModels/RankedResult.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace ViretTool.SimilarityModels
 {
-    class RankedFrame : IComparable
+    // TODO - Change class to struct if possible
+    //        >> RankedFrame will then be considered as value type (not reference type)
+    //        >> One pointer jump will be saved for every op with the struct
+    //        >> But the Rank value will behave as regular double (thus edit after copy will not effect the value of the original struct)
+    class RankedFrame : IComparable<RankedFrame>
     {
         public readonly DataModel.Frame Frame;
         public double Rank;
@@ -17,21 +21,22 @@ namespace ViretTool.SimilarityModels
             Rank = rank;
         }
 
-        public int CompareTo(object obj)
+        public int CompareTo(RankedFrame other)
         {
-            RankedFrame rf = (RankedFrame)obj;
-            return Rank.CompareTo(rf.Rank);
+            return Rank.CompareTo(other.Rank);
         }
 
         public static List<RankedFrame> InitializeResultList(DataModel.Dataset dataset)
         {
             List<RankedFrame> result = new List<RankedFrame>();
+            result.Capacity = dataset.Frames.Count;
 
             foreach (DataModel.Frame frame in dataset.Frames)
                 result.Add(new RankedFrame(frame, 0));
 
             return result;
         }
+
 
     }
 }
