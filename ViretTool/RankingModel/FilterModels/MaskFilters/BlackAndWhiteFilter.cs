ebe2fb90af3443c30174588d61a026fdabed866f
Tomáš Souček <soucek.gns@gmail.com>
2017-11-14 14:38:04 +0100

    B/W filter.

---
+++ ViretTool/RankingModel/FilterModels/MaskFilters/BlackAndWhiteFilter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViretTool.DataModel;
+
+namespace ViretTool.RankingModel.FilterModels.MaskFilters {
+    class BlackAndWhiteFilter : MaskFilter {
+
+        public BlackAndWhiteFilter(Dataset dataset) : base(dataset) {
+            string filename = dataset.AllExtractedFramesFilename.Split('-')[0] + ".bwfilter";
+
+            LoadFromFile(filename, new bool[dataset.Frames.Count]);
+        }
+
+        private void LoadFromFile(string filename, bool[] mask) {
+
+            using (var stream = new BinaryReader(File.OpenRead(filename))) {
+                // header = 'BWfilter'
+                if (stream.ReadInt64() != 0x7265746c69665742)
+                    throw new FileFormatException("Invalid bwfilter file format.");
+                // threshold number
+                stream.ReadInt32();
+
+                while (stream.BaseStream.Position != stream.BaseStream.Length) {
+                    int frameId = stream.ReadInt32();
+                    mask[frameId] = true;
+                }
+            }
+
+            Mask = mask;
+        }
+    }
+}
