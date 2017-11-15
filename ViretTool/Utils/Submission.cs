646f953fab222677827692ea8cd7cdbb0dbb54c4
Tomáš Souček <soucek.gns@gmail.com>
2017-11-15 15:50:46 +0100

    Submission class.

---
+++ ViretTool/Utils/Submission.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViretTool.Utils {
+    class Submission {
+
+        public Submission() {
+            IP = new IPAddress(new byte[] { 0, 0, 0, 0 });
+        }
+
+        public bool IsConnected { get; private set; }
+
+        /// <summary>
+        /// Never null, can be always shown by IP.ToString().
+        /// </summary>
+        public IPAddress IP { get; private set; }
+
+        public int Port { get; private set; }
+
+        /// <summary>
+        /// Null if not set.
+        /// </summary>
+        public string TeamName { get; private set; }
+
+        public void Connect(string ip, int port, string teamName = null) {
+
+        }
+
+        public void Send(int trecvidVideoId, int trecvidFrameId) {
+
+        }
+
+        public void Disconnect() {
+            // TODO disconnect
+
+            Port = 0;
+            IsConnected = false;
+            IP = new IPAddress(new byte[] { 0, 0, 0, 0 });
+        }
+
+    }
+}
