0cdd803fa55ded6bf374d39dd388fd724432f337
Gregor Kovalcik <gregor.kovalcik@gmail.com>
2017-11-24 17:31:21 +0100

    Added global exception handler and logger.

--- ViretTool/App.xaml.cs
+++ ViretTool/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using ViretTool.BasicClient;
 
 namespace ViretTool
 {
@@ -13,5 +14,21 @@ namespace ViretTool
     /// </summary>
     public partial class App : Application
     {
+        public App() : base()
+        {
+            this.Dispatcher.UnhandledException += OnDispatcherUnhandledException;
+        }
+
+        void OnDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            string errorMessage = string.Format("An unhandled exception occurred: {0}", e.Exception.Message);
+            Logger.Log(sender, Severity.Fatal, errorMessage);
+            Logger.Log(sender, Severity.Fatal, e.Exception.ToString());
+
+            MessageBox.Show(errorMessage, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
     }
+
+
 }
